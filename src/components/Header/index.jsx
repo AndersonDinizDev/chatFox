3f8124971c6e50a0b9e6545d12576aaea224b419
andersondinizdev <andersondiniz159@gmail.com>
2024-04-13 19:25:10 -0300

    Estilização do header.
    - Adicionado logo, links e botões de login/cadastro;

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,9 +1,58 @@
-import React from "react";
+import React, { useState } from "react";
+
+import Logo from "../../assets/logo.svg";
 
 import { Header as HeaderComponent } from "./styles";
+import { LogoDiv, ListLink, ButtonsDiv } from "./styles";
 
-const Header = () => {
-  return <HeaderComponent>teste</HeaderComponent>;
+const Header = ({ ...props }) => {
+  const [buttonLoginSelected, setButtonLoginSelected] = useState(false);
+  const [buttonRegisterSelected, setButtonRegisterSelected] = useState(true);
+
+  return (
+    <HeaderComponent {...props}>
+      <LogoDiv>
+        <img src={Logo} alt="logo" />
+        <h2>ChatFox</h2>
+      </LogoDiv>
+      <ListLink>
+        <li>
+          <button>Home</button>
+        </li>
+        <li>
+          <button>Chat</button>
+        </li>
+      </ListLink>
+      <ButtonsDiv>
+        <button
+          onClick={() => {
+            setButtonLoginSelected(true);
+            setButtonRegisterSelected(false);
+          }}
+          style={{
+            backgroundColor: buttonLoginSelected ? "#7f56d9" : "transparent",
+            color: buttonLoginSelected ? "#fff" : "#475467",
+            border: buttonLoginSelected ? "1px solid #7f56d9" : "none",
+          }}
+        >
+          Login
+        </button>
+        <button
+          onClick={() => {
+            setButtonRegisterSelected(true);
+            setButtonLoginSelected(false);
+          }}
+          style={{
+            backgroundColor: buttonRegisterSelected ? "#7f56d9" : "transparent",
+            color: buttonRegisterSelected ? "#fff" : "#475467",
+            border: buttonRegisterSelected ? "1px solid #7f56d9" : "none",
+          }}
+        >
+          Cadastro
+        </button>
+      </ButtonsDiv>
+    </HeaderComponent>
+  );
 };
 
 export default Header;
--- src/components/Header/styles.jsx
+++ src/components/Header/styles.jsx
@@ -1,3 +1,56 @@
 import styled from "styled-components";
 
-export const Header = styled.header``;
+export const Header = styled.header`
+  display: flex;
+  gap: 2rem;
+  align-items: center;
+  border-bottom: 1px solid #f2f4f7;
+  padding: 1rem;
+`;
+
+export const LogoDiv = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 1rem;
+`;
+
+export const ListLink = styled.ul`
+  display: flex;
+  align-items: center;
+  list-style: none;
+  gap: 2rem;
+  margin-left: 2rem;
+
+  button {
+    background-color: transparent;
+    border: none;
+    outline: none;
+    font-size: 16px;
+    font-weight: 600;
+    line-height: 24px;
+    color: #475467;
+    cursor: pointer;
+
+    &:hover {
+      color: #000;
+    }
+  }
+`;
+
+export const ButtonsDiv = styled.div`
+  display: flex;
+  gap: 0.5rem;
+  align-items: center;
+  margin-left: auto;
+
+  button {
+    padding: 10px 18px;
+    box-shadow: 0px 1px 2px 0px rgba(16, 24, 40, 0.05);
+    border-radius: 8px;
+    font-size: 16px;
+    font-weight: 600;
+    line-height: 24px;
+    color: #fff;
+    cursor: pointer;
+  }
+`;
